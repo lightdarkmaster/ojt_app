a2728731c8be7da30dd3465166e7c4bf86ced630
darkMasterv <79690406+lightdarkmaster@users.noreply.github.com>
2025-05-17 14:04:34 +0800

    Add Functionalities to calculate rendered hours etc.

--- lib/pages/ojt_hourSolver.dart
+++ lib/pages/ojt_hourSolver.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HourSolver extends StatefulWidget {
   const HourSolver({super.key});
@@ -9,10 +10,22 @@ class HourSolver extends StatefulWidget {
 
 class _HourSolverState extends State<HourSolver> {
   final TextEditingController _hoursController = TextEditingController();
+  double _remainingHours = 0.0;
+  double _remainingDays = 0.0;
+  DateTime _estimatedDate = DateTime.now();
 
   void _calculateHours() {
     const totalHours = 486.0;
-    double hours = double.tryParse(_hoursController.text) ?? 0.0;
+    double renderedhours = double.tryParse(_hoursController.text) ?? 0.0;
+    if (renderedhours == 0) {
+      _estimatedDate = DateTime.now();
+    } else {
+      _remainingHours = totalHours - renderedhours;
+      _remainingDays = _remainingHours / 8;
+      final remainingDays = _remainingHours / 8;
+      _estimatedDate = DateTime.now().add(Duration(days: remainingDays.round()));
+    }
+    setState(() {});
   }
 
   @override
@@ -33,30 +46,62 @@ class _HourSolverState extends State<HourSolver> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: const Text(
-                'Rendered Hours',
-                style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: const Text(
+                  'Rendered Hours',
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            TextField(
-              controller: _hoursController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Enter hours',
+              TextField(
+                controller: _hoursController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter hours',
+                ),
+                keyboardType: TextInputType.number,
               ),
-              keyboardType: TextInputType.number,
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: _calculateHours,
-              child: const Text('Calculate'),
-            ),
-          ],
+              const SizedBox(height: 16.0),
+              Center(
+                child: ElevatedButton(
+                  onPressed: _calculateHours,
+                  child: const Text('Calculate'),
+                ),
+              ),
+              Center(
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'Note: Assuming You will render 8 hours per day',
+                      style: const TextStyle(fontSize: 16.0, fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              Center(
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Text(
+                        'Remaining Hours: ${_remainingHours.toStringAsFixed(2)} hours\n'
+                        'Remaining Days: ${_remainingDays.round()} Day/s\n'
+                        'Estimated finish date: ${DateFormat('MMMM dd, yyyy').format(_estimatedDate)}',
+                        style: const TextStyle(fontSize: 16.0, color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
